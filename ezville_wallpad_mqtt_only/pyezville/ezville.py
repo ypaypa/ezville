22097dd832e44e5099babb0f0707ea71a22560b4
ktdo79 <98633736+ktdo79@users.noreply.github.com>
2022-03-20 11:46:56 +0900

    Update ezville.py

--- ezville_wallpad_mqtt_only/pyezville/ezville.py
+++ ezville_wallpad_mqtt_only/pyezville/ezville.py
@@ -744,6 +744,7 @@ def do_work(config):
             
 #        while True:
         while start_flag:
+            log("RRRRRRRR")
             try:
 #                if time.time_ns() - COLLECTDATA['LastRecv'] > 10000000000:  # 10s
 #                if time.time_ns() - COLLECTDATA['LastRecv'] > 100000000000: 
