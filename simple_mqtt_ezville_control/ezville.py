1344980e37c1353caa5d435055bd3ead614b8a50
ypaypa <ypaypa@gmail.com>
2025-05-04 09:16:12 +0900

    Update ezville.py

--- simple_mqtt_ezville_control/ezville.py
+++ simple_mqtt_ezville_control/ezville.py
@@ -976,6 +976,8 @@ def ezville_loop(config):
 
         
     # MQTT 통신
+    from paho.mqtt.enums import CallbackAPIVersion
+    mqtt_client = mqtt.Client(CallbackAPIVersion.VERSION1, 'mqtt-ezville')
     mqtt_client = mqtt.Client('mqtt-ezville')
     mqtt_client.username_pw_set(config['mqtt_id'], config['mqtt_password'])
     mqtt_client.on_connect = on_connect
